4319dade66bf92d847dbdef9e54213a195cdd765
Paul Warren <pwarren@pivotal.io>
2020-06-08 16:27:03 -0700

    Refactor install-cf-on-gke to be able to handle upgrade as well as fresh install [#171631154](https://www.pivotaltracker.com/story/show/171631154)
    Co-authored-by: Joseph Palermo <jpalermo@pivotal.io>

--- ci/tasks/install-cf-on-gke/task.sh
+++ ci/tasks/install-cf-on-gke/task.sh
@@ -9,9 +9,17 @@ gcloud container clusters get-credentials "${cluster_name}" --zone ${GCP_PROJECT
 DNS_DOMAIN="${cluster_name}.k8s-dev.relint.rocks"
 cf-for-k8s-candidate/hack/confirm-network-policy.sh "${cluster_name}" ${GCP_PROJECT_ZONE}
 
-echo "Generating install values..."
-cf-for-k8s-candidate/hack/generate-values.sh --cf-domain "${DNS_DOMAIN}" --gcr-service-account-json gcp-service-account.json > cf-install-values.yml
-echo "istio_static_ip: $(jq -r '.lb_static_ip' pool-lock/metadata)" >> cf-install-values.yml
+if [[ "${UPGRADE}" == "true" ]]; then
+  echo "Copying bosh vars store from latest release install"
+  mkdir -p "/tmp/${cluster_name}.k8s-dev.relint.rocks"
+  cp env-metadata/cf-vars.yaml "/tmp/${cluster_name}.k8s-dev.relint.rocks/cf-vars.yaml"
+  echo "Using install values from the last install..."
+  cp env-metadata/last-cf-install-values.yml cf-install-values.yml
+else
+  echo "Generating install values..."
+  cf-for-k8s-candidate/hack/generate-values.sh --cf-domain "${DNS_DOMAIN}" --gcr-service-account-json gcp-service-account.json > cf-install-values.yml
+  echo "istio_static_ip: $(jq -r '.lb_static_ip' pool-lock/metadata)" >> cf-install-values.yml
+fi
 
 echo "Installing CF..."
 kapp deploy -a cf -f <(ytt -f cf-for-k8s-candidate/config -f cf-install-values.yml) -y
@@ -19,4 +27,4 @@ kapp deploy -a cf -f <(ytt -f cf-for-k8s-candidate/config -f cf-install-values.y
 bosh interpolate --path /cf_admin_password cf-install-values.yml > env-metadata/cf-admin-password.txt
 echo "${DNS_DOMAIN}" > env-metadata/dns-domain.txt
 cp "/tmp/${cluster_name}.k8s-dev.relint.rocks/cf-vars.yaml" env-metadata
-cp cf-install-values.yml env-metadata/original-cf-install-values.yml
+cp cf-install-values.yml env-metadata/last-cf-install-values.yml
