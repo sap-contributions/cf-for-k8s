c96066b2b45791b5ce80ddbe0897056f6ea01046
Paul Warren <pwarren@pivotal.io>
2020-06-08 14:45:58 -0700

    Refactor task for installing cf on gke
    - rename cf-for-k8s input and ensure env vars are used properly
    [#171631154](https://www.pivotaltracker.com/story/show/171631154)
    
    Co-authored-by: Joseph Palermo <jpalermo@pivotal.io>

--- ci/tasks/install-cf-on-gke/task.sh
+++ ci/tasks/install-cf-on-gke/task.sh
@@ -1,20 +1,20 @@
-#!/bin/bash -eu
+#!/bin/bash -eux
 
 export KUBECONFIG=kube-config.yml
 cluster_name=$(cat pool-lock/name)
-echo '${GCP_SERVICE_ACCOUNT_JSON}' > gcp-service-account.json
-gcloud auth activate-service-account --key-file=gcp-service-account.json --project='${GCP_PROJECT_NAME}' >/dev/null 2>&1
-gcloud container clusters get-credentials "${cluster_name}" --zone '${GCP_PROJECT_ZONE}' >/dev/null 2>&1
+echo ${GCP_SERVICE_ACCOUNT_JSON} > gcp-service-account.json
+gcloud auth activate-service-account --key-file=gcp-service-account.json --project=${GCP_PROJECT_NAME} >/dev/null 2>&1
+gcloud container clusters get-credentials "${cluster_name}" --zone ${GCP_PROJECT_ZONE} >/dev/null 2>&1
 
 DNS_DOMAIN="${cluster_name}.k8s-dev.relint.rocks"
-cf-for-k8s-latest-candidate/hack/confirm-network-policy.sh "${cluster_name}" '${GCP_PROJECT_ZONE}'
+cf-for-k8s-candidate/hack/confirm-network-policy.sh "${cluster_name}" ${GCP_PROJECT_ZONE}
 
 echo "Generating install values..."
-cf-for-k8s-latest-candidate/hack/generate-values.sh --cf-domain "${DNS_DOMAIN}" --gcr-service-account-json gcp-service-account.json > cf-install-values.yml
+cf-for-k8s-candidate/hack/generate-values.sh --cf-domain "${DNS_DOMAIN}" --gcr-service-account-json gcp-service-account.json > cf-install-values.yml
 echo "istio_static_ip: $(jq -r '.lb_static_ip' pool-lock/metadata)" >> cf-install-values.yml
 
 echo "Installing CF..."
-kapp deploy -a cf -f <(ytt -f cf-for-k8s-latest-candidate/config -f cf-install-values.yml) -y
+kapp deploy -a cf -f <(ytt -f cf-for-k8s-candidate/config -f cf-install-values.yml) -y
 
 bosh interpolate --path /cf_admin_password cf-install-values.yml > env-metadata/cf-admin-password.txt
 echo "${DNS_DOMAIN}" > env-metadata/dns-domain.txt
